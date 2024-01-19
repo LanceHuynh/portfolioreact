1ddf6850ebbd985bece54047e48baf11294bf82e
LanceHuynh <34030826+LanceHuynh@users.noreply.github.com>
2024-01-19 22:16:08 +0100

    add dockerfile

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './component/Homepage/Homepage';
 import Header from './component/Header/Header';
 import About from './component/About/About';
