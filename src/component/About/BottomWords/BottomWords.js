c830abc93aeab2bb01123072c6c65c406af521bc
LanceHuynh <34030826+LanceHuynh@users.noreply.github.com>
2024-01-20 00:02:59 +0100

    done for real again

--- src/component/About/BottomWords/BottomWords.js
+++ src/component/About/BottomWords/BottomWords.js
@@ -2,13 +2,14 @@
 import React from 'react';
 import useScrollPopUp from 'component/Animation/useScrollPopUp'; // Adjust the path if necessary
 import './BottomWords.css'; // Assuming this is the CSS file for BottomWords component
+import { useRouteContext } from 'component/Context/RouteContext';
 
 const BottomWords = () => {
-
+  const {toggleElement} = useRouteContext(); 
   const { ref: bottomRef, animate: bottomAnimate } = useScrollPopUp();
   return (
     <div ref={bottomRef} className={`bottom-words ${bottomAnimate ? "go" : ""}`}>
-      <p>Want to know more? <a href="/contact">Get in touch!</a></p>
+      <p>Want to know more? <a onClick={()=>{toggleElement('C')}}>Get in touch!</a></p>
     </div>
   );
 };
